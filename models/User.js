25a06f3bf43edc5d569df16bd8035dea8c3d33c3
Remi Boivin <remi.boivin@epitech.eu>
2022-09-24 16:21:59 +0200

    add user and school model

--- models/User.js
+++ models/User.js
@@ -1,21 +1,35 @@
 import mongoose from 'mongoose'
+import { ObjectId } from "mongoose"
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: [true, 'Please provide a first name.'],
+    maxlenght: [20, 'First name cannot be more than 20 characters'],
   },
   lastName: {
     type: String,
-  },
-  password: {
-    type: String,
+    required: [true, 'Please provide a last name.'],
+    maxlenght: [20, 'Last name cannot be more than 20 characters'],
   },
   email: {
     type: String,
+    required: [true, 'Please provide a email.'],
+    maxlenght: [20, 'Email cannot be more than 20 characters'],
   },
-  school: {
-    type: Array,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+  },
+  phoneNumber: {
+    type: Number,
+    required: [true, 'Please provide a phone number.'],
+    maxlenght: [20, 'Phone number cannot be more than 20 characters'],
+  },
+  schoolId: {
+    type: ObjectId,
+    required: [true],
     },
 })
 
