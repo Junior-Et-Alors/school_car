a90278375e2979eefe7ff9bbea7533ba8ba7bd6e
Remi Boivin <remi.boivin@epitech.eu>
2022-09-24 16:21:59 +0200

    add user and school model

--- models/School.js
+++ models/School.js
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose'
+import { ObjectId } from "mongoose"
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
 const SchoolSchema = new mongoose.Schema({
   name: {
     type: String,
   },
-  Address: {
+  address: {
     type: String,
   },
   phoneNumber: {
     type: Number,
   },
+  riderId: {
+    type: [{ type: ObjectId }],
+    },
 })
 
 export default mongoose.models.School || mongoose.model('School', SchoolSchema)
